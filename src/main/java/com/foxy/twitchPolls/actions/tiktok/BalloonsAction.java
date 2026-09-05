876d8ac42a5b35d37ab0486d4858d66517a3ac2a
FoxyLeo <leitosotelo222@gmail.com>
2026-09-04 21:48:49 -0300

    Add BalloonsAction and localize action bar messages for English and Spanish

--- src/main/java/com/foxy/twitchPolls/actions/tiktok/BalloonsAction.java
+++ src/main/java/com/foxy/twitchPolls/actions/tiktok/BalloonsAction.java
@@ -1,5 +1,6 @@
 package com.foxy.twitchPolls.actions.tiktok;
 
+import com.foxy.twitchPolls.TwitchPolls;
 import com.foxy.twitchPolls.actions.ActionContext;
 import com.foxy.twitchPolls.actions.ActionStrategy;
 import org.bukkit.FireworkEffect;
@@ -7,14 +8,20 @@ import org.bukkit.Location;
 import org.bukkit.Particle;
 import org.bukkit.Sound;
 import org.bukkit.entity.Firework;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import org.bukkit.inventory.meta.FireworkMeta;
 import org.bukkit.scheduler.BukkitRunnable;
 
 public class BalloonsAction implements ActionStrategy {
     @Override
     public void execute(ActionContext context) {
+        TwitchPolls plugin = (TwitchPolls) context.plugin();
         long durationTicks = Math.max(1L, context.config().getLong("duration-seconds", 10L) * 20L);
         double riseSpeed = Math.max(0.0, context.config().getDouble("rise-speed", 0.08));
+        boolean wasAllowFlight = context.player().getAllowFlight();
+        boolean wasFlying = context.player().isFlying();
+        context.player().setAllowFlight(true);
+        context.player().setFlying(false);
         Location feet = context.player().getLocation().clone().subtract(0.0, 0.25, 0.0);
         Firework firework = context.world().spawn(feet, Firework.class);
         FireworkMeta fireworkMeta = firework.getFireworkMeta();
@@ -34,11 +41,21 @@ public class BalloonsAction implements ActionStrategy {
             @Override
             public void run() {
                 if (!context.player().isOnline() || ticks++ >= durationTicks) {
+                    if (context.player().isOnline()) {
+                        context.player().setFlying(wasFlying);
+                        context.player().setAllowFlight(wasAllowFlight);
+                        context.player().sendActionBar(LegacyComponentSerializer.legacyAmpersand().deserialize(""));
+                    }
                     cancel();
                     return;
                 }
 
                 Location current = context.player().getLocation();
+                int remainingSeconds = (int) Math.ceil((durationTicks - ticks) / 20.0);
+                String actionbar = plugin.getLanguageManager().getString(
+                    "messages.balloons-actionbar", "&eBalloons: &f%time%s")
+                    .replace("%time%", String.valueOf(remainingSeconds));
+                context.player().sendActionBar(LegacyComponentSerializer.legacyAmpersand().deserialize(actionbar));
                 Location next = current.clone();
                 next.setY(Math.min(current.getY() + riseSpeed, current.getWorld().getMaxHeight() - 1.0));
                 context.player().teleport(next);
